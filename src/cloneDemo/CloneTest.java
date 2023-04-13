8cdd4a1eb3e8cbc1572bdbd2bc3ff5c33adca0d7
Ahmad Farhad Habib <Ahmad Farhad Habib@10.0.0.21>
2023-04-12 21:38:51 -0400

    clone test demo

---
+++ src/cloneDemo/CloneTest.java
@@ -0,0 +1,16 @@
+package cloneDemo;
+
+public class CloneTest {
+
+	public static void main(String[] args) {
+		System.out.println("CloneTest");
+
+	}
+	public void add () {
+		System.out.println("2+5");
+	}
+	public void subtract () {
+		System.out.println("5-2");
+	}
+
+}
